2ffb095b194fae5f3254f499f5509e64dea312c0
Nima Araghi <nims.844@gmail.com>
2025-08-12 00:15:32 +0330

    added the suspense ot login and signup

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,16 +1,18 @@
 import { auth } from "@/auth";
 import LoginForm from "@/features/users/components/LoginForm";
 import { redirect } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 
-export default async function Login() {
-  const session = await auth();
-
-  if (session) redirect("/dashboard");
-
+export default function Login() {
   return (
-    <>
-      <LoginForm />
-    </>
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginWithSessionCheck />
+    </Suspense>
   );
+}
+
+async function LoginWithSessionCheck() {
+  const session = await auth();
+  if (session) redirect("/dashboard");
+  return <LoginForm />;
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,16 +1,20 @@
 import { auth } from "@/auth";
 import SignupForm from "@/features/users/components/SignupForm";
 import { redirect } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 
-export default async function Signup() {
+export default function Signup() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SignupWithSession />
+    </Suspense>
+  );
+}
+
+async function SignupWithSession() {
   const session = await auth();
 
   if (session) redirect("/dashboard");
 
-  return (
-    <>
-      <SignupForm />
-    </>
-  );
+  return <SignupForm />;
 }
